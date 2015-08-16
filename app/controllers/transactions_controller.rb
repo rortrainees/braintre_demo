09f237a98d0dd016a0baedbcaae681168bdb5344
amitattri <attriamit8@gmail.com>
2015-08-16 22:56:03 +0530

    basic servise

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,36 @@
+class TransactionsController < ApplicationController
+
+	def index
+
+	end	
+
+  def new
+    @client_token = Braintree::ClientToken.generate
+
+    #braintree.setup("#{@client_token}", "custom", {id: "my_form"});
+  end
+
+
+
+ 
+  def create
+    puts"++++++++++params[:payment_method_nonce]++++#{params[:payment_method_nonce].inspect}+++++++++++"
+    @client_token = Braintree::ClientToken.generate
+    #braintree.setup("#{@client_token}", "custom", {id: "my_form"});
+    @result = Braintree::Transaction.sale(
+              amount:"100.00",
+              payment_method_nonce: params[:payment_method_nonce])
+    if @result.success?
+      redirect_to root_path , flash[:notice] = "Congraulations! Your transaction has been successfully!"
+    else
+      flash[:error] = result.message
+      render :new
+    end
+  end
+  private
+  def generate_client_token
+    Braintree::ClientToken.generate
+  end
+end
+
+  
---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = ' ' #merchant id of your account
+Braintree::Configuration.public_key = '' #public key of your sandbox account 
+Braintree::Configuration.private_key = '' #private key of your sandbox account 
